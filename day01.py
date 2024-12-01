8b02a86db7795b8144335e61e13b53d713e9b076
Tuukka Ruhanen <tuukka.t.ruhanen@gmail.com>
2024-12-01 21:54:41 +0200

    Day 1

--- day01.py
+++ day01.py
@@ -1,13 +1,31 @@
-from urllib.request import urlopen
+from collections import Counter
+from pathlib import Path
 
-INPUT_URL = "https://adventofcode.com/2024/day/1/input"
+
+def solve(values1: list[int], values2: list[int]) -> tuple[int, int]:
+    distance_sum = sum(
+        abs(value1 - value2) for value1, value2 in zip(sorted(values1), sorted(values2))
+    )
+
+    counts2 = Counter(values2)
+    similarity_score = sum(
+        value1 * counts2[value1] if value1 in counts2 else 0 for value1 in values1
+    )
+
+    return distance_sum, similarity_score
+
 
 def main():
-    data = urlopen(INPUT_URL)
-    list1 = []
-    list2 = []
-    distance_sum = sum(abs(value1 - value2) for value1, value2 in zip(sorted(list1), sorted(list2)))
-    return distance_sum
+    input_data_path = Path(__file__).parent / "input_data" / "day01.txt"
+    input_data = input_data_path.read_text()
+    rows = input_data.split("\n")
+    pairs = [tuple(int(v) for v in row.split("   ")) for row in rows]
+    values1, values2 = zip(*pairs)
+
+    distance_sum, similarity_score = solve(values1, values2)
+
+    print(f"Distance sum: {distance_sum}\nSimilarity score: {similarity_score}")
+
 
 if __name__ == "__main__":
     main()
